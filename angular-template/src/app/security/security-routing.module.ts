2b0f0f932f512d39e3b2c9f9f128d9642f73d114
Max Andreassen <andreassenmax@gmail.com>
2020-11-12 12:40:58 +0000

    - Bootstrap 4, font awesome and login page added

--- angular-template/src/app/security/security-routing.module.ts
+++ angular-template/src/app/security/security-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SecurityComponent } from './security.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: SecurityComponent },
+  { path: '', component: LoginComponent },
   { path: ':id', component: SecurityComponent },
 ];
 
--- angular-template/src/app/security/security.module.ts
+++ angular-template/src/app/security/security.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecurityComponent } from './security.component';
 import { SecurityRoutingModule } from './security-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SecurityRoutingModule
   ],
-  declarations: [SecurityComponent]
+  declarations: [
+    SecurityComponent,
+    LoginComponent
+  ]
 })
 export class SecurityModule { }
